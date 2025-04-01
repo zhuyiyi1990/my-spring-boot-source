cc3f0405e9b6dc9e562991171c06426cd7ee0a58
朱一一 <zhuyiyi1990@outlook.com>
2025-04-01 09:15:37 +0800

    zhuyiyi

--- src/main/java/com/github/zhuyiyi1990/boot/Application.java
+++ src/main/java/com/github/zhuyiyi1990/boot/Application.java
@@ -2,12 +2,16 @@ package com.github.zhuyiyi1990.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+        ApplicationContext parentContext = context.getParent();
+        System.out.println("context = " + context);
+        System.out.println("parentContext = " + parentContext);
     }
 
 }
